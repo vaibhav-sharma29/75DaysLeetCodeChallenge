5baa8154aca9528d12f218e5e5e3f760f6a440c2
Vaibhav Sharma <vaibhavkrish299@gmail.com>
2026-03-12 21:51:10 +0530

    Time: 44 ms (35.01%), Space: 46.7 MB (94.98%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Pehla loop: Ek number uthane ke liye
         for (int i = 0; i < nums.length; i++) {
             
-            // Dusra loop: Uske aage wale numbers se check karne ke liye
+           
             for (int j = i + 1; j < nums.length; j++) {
-                
-                // Agar dono ka sum target ke barabar hai
                 if (nums[i] + nums[j] == target) {
                     return new int[] { i, j };
                 }
             }
         }
-        
-        // Agar kuch na mile (Question ke hisab se ye line kabhi hit nahi hogi)
         return new int[] {};
     }
 }
